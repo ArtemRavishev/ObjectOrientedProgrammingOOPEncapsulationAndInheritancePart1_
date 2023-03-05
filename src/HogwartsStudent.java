9a77992ddf10122ff061119e6a7418df083c308b
ArtemRavishev <ravishev@inbox.ru>
2023-03-05 16:05:07 +0300

    ДЗ по объектно-ориентированному программированию (ООП)

---
+++ src/HogwartsStudent.java
@@ -0,0 +1,58 @@
+public abstract class HogwartsStudent {
+
+    private final String nameAndSurname;
+
+    private final int magic;
+
+    private final int transgression;
+
+    public HogwartsStudent(String nameAndSurname, int magic, int transgression) {
+        this.nameAndSurname = nameAndSurname;
+        this.magic = magic;
+        this.transgression = transgression;
+    }
+
+    public String getNameAndSurname() {
+        return nameAndSurname;
+    }
+
+
+
+    public int getMagic() {
+        return magic;
+    }
+
+
+    public int getTransgression() {
+        return transgression;
+    }
+
+
+
+    private int ability() {
+        return magic + transgression;
+    }
+
+    public void compareHogwarts (HogwartsStudent hogwartsStudent ) {
+        int ability1 = ability();
+        int ability2 = hogwartsStudent.ability();
+        if (ability1 > ability2) {
+            System.out.println("Студент " + getNameAndSurname() + " лучше чем студент " + hogwartsStudent.getNameAndSurname() + " : " + ability1 + " VS " + ability2);
+        } else if (ability1 < ability2) {
+            System.out.println("Студент  " + getNameAndSurname() + " лучше чем студент " + hogwartsStudent.getNameAndSurname() + " : " + ability1 + " VS " + ability2);
+        } else {
+            System.out.println("Студент " + getNameAndSurname() + " точно такой же, как студент " + hogwartsStudent.getNameAndSurname() + " : " + ability1 + " VS " + ability2);
+
+        }
+    }
+
+    public void print() {
+        System.out.println(this);
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Студент %s; Сила магиии: %d; Сила трансгрессии: %d", nameAndSurname, magic, transgression);
+    }
+}
